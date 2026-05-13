f2c08925a933aaa45460fb9bc49cfc4dfe4c8b1d
松鼠 <1378ginger@gmail.com>
2026-05-13 17:40:24 +0800

    test

---
+++ test1.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+a = np.array([1, 2, 3])
+
+print(a.shape)
+
+b = np.array([[1, 2, 3],
+
+        [4, 5, 6]])
+
+print(b.shape)
+
+
+c = np.array([[[1, 2],
+
+               [3, 4]]])
+
+print(c.shape)
